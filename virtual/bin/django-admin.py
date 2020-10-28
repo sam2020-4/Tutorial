7b9c5930126d25d4d23eb61f921c34cd7c772a71
matata samuel <mattasamuel3@gmail.com>
2020-10-28 12:15:25 +0300

    initial commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Desktop/moringa-prj/core/Django/Tutorial/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
